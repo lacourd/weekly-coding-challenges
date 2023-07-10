bb7baba57a80328f68985604d9e8ac875f0e54fc
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-07-10 13:07:25 -0500

    Completed Week 2 Challenge

--- Week2_Challenge.js
+++ Week2_Challenge.js
@@ -1,1 +1,11 @@
-Week 1 challenge
+function FirstReverse(str) { 
+	let newStr ="";
+	for (i = 1; i < str.length + 1; i++) {
+	  newStr += str[str.length-i]
+	}
+	return newStr; 
+
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
