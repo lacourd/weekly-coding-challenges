57d4b669e40e85d6b73c601091212372158c41c8
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-07-17 12:06:21 -0500

    Week 3 Challenge completed

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,1 +1,26 @@
-Week 1 challenge
+function LongestWord(sen) { 
+
+  // code goes here 
+  let validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let words = sen.split(' ');
+  let longest = "";
+  for (let i=0; i<words.length; i++) {
+    let currentWord = words[i];
+    let characterCount = 0;
+    for (let j=0; j<currentWord.length; j++) {
+      if (validCharacters.includes(currentWord[j])) {
+        characterCount++
+      } else {
+        characterCount--
+      }
+      }
+      if (characterCount > longest.length) {
+        longest = currentWord;
+    }
+  }
+  return longest; 
+
+}
+   
+// keep this function call here 
+console.log(LongestWord(readline()));
