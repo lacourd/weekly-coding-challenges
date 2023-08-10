764c699af92abf3335cb780e235d569a7a878a9e
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-08-10 09:49:04 -0500

    Week 5 Challenge completed

--- Week5_Challenge.js
+++ Week5_Challenge.js
@@ -1,1 +1,19 @@
-Week 1 challenge
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+		let sentence = "";
+		if (this.age > other.age) {
+			sentence = other.name + " is younger than me.";
+		} else if (this.age < other.age) {
+			sentence = other.name + " is older than me.";
+		} else {
+			sentence = other.name + " is the same age as me.";
+		}
+		return sentence;
+	}
+
+}
